6ecc15b60bee26f0a3772bc6f34338d990c7149a
Roonie006 <90405983+Roonie006@users.noreply.github.com>
2021-10-06 17:37:13 +0530

    Update channelmusic.py

--- VCPlayBot/helpers/channelmusic.py
+++ VCPlayBot/helpers/channelmusic.py
@@ -2,6 +2,6 @@ from pyrogram.types import Chat
 
 
 def get_chat_id(chat: Chat):
-    if chat.title.startswith("Channel Music: ") and chat.title[16:].isnumeric():
+    if chat.title.startswith("Chanel Music: ") and chat.title[16:].isnumeric():
         return int(chat.title[15:])
     return chat.id
