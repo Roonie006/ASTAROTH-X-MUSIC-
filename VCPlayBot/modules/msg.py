ad31bbfa2394faea4ce05f41f9c1a30ad7c1335b
Roonie <90405983+Roonie006@users.noreply.github.com>
2021-10-29 17:09:34 +0530

    Update msg.py

--- VCPlayBot/modules/msg.py
+++ VCPlayBot/modules/msg.py
@@ -5,7 +5,7 @@ from VCPlayBot.config import PROJECT_NAME
 from VCPlayBot.config import SUPPORT_GROUP
 from VCPlayBot.config import UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info.\n\n Join @Girls_And_Boys_Chatting"
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info.\n\n Join @astarothxsupport"
       HELP_MSG = [
         ".",
 f"""
@@ -15,7 +15,7 @@ f"""
 
 ⚪️ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
 
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """,
 
 f"""
@@ -31,7 +31,7 @@ f"""
 2) Send /userbotjoinchannel in linked group
 3) Now send commands in linked group
 
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """,
 f"""
 **Commands**
@@ -83,7 +83,7 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 3) Add bot as Channel admin with full perms
 4) Add @{ASSISTANT_NAME} to the channel as an admin.
 5) Simply send commands in your group. (remember to use /ytplay instead /play)
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """,
 
 f"""
@@ -92,7 +92,7 @@ f"""
 - /musicplayer [on/off]: Enable/Disable Music player
 - /admincache: Updates admin info of your group. Try if bot isn't recognize admin
 - /userbotjoin: Invite @{ASSISTANT_NAME} Userbot to your chat
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """,
 f"""
 **=>> Song Download 🎸**
@@ -106,7 +106,7 @@ f"""
 
 - /search [song name]: Search youtube for songs
 - /lyrics [song name]: Get song lyrics
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """,
 
 f"""
@@ -116,6 +116,6 @@ f"""
  - /broadcast <reply to message> - globally brodcast replied message to all chats
  - /pmpermit [on/off] - enable/disable pmpermit message
 *Sudo Users can execute any command in any groups
-Join @Girls_And_Boys_Chatting
+Join @astarothxsupport
 """
       ]
